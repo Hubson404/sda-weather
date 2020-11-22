e3ba688a84cf40e8f2e808d6baf67a9fc21d21ae
Hubson404 <60826441+Hubson404@users.noreply.github.com>
2020-11-22 11:40:04 +0100

    Added some tests

--- src/test/java/org/hubson404/weather/localization/LocationCreateServiceTest.java
+++ src/test/java/org/hubson404/weather/localization/LocationCreateServiceTest.java
@@ -53,12 +53,72 @@ class LocationCreateServiceTest {
     }
 
     @Test
+    void createLocation_whenCityNameIsWhitespaceOnly_throwsInsufficientDataException() {
+        // when
+        Throwable result = catchThrowable(() -> locationCreateService.createLocation(new LocationDefinition(
+                "   ",
+                0,
+                0,
+                "",
+                "country"
+        )));
+        // then
+        assertThat(result).isExactlyInstanceOf(InsufficientDataException.class);
+        verify(locationRepository, times(0)).save(any(Location.class));
+    }
+
+    @Test
+    void createLocation_whenCountryNameIsEmpty_throwsInsufficientDataException() {
+        // when
+        Throwable result = catchThrowable(() -> locationCreateService.createLocation(new LocationDefinition(
+                "city-name",
+                0,
+                0,
+                "",
+                ""
+        )));
+        // then
+        assertThat(result).isExactlyInstanceOf(InsufficientDataException.class);
+        verify(locationRepository, times(0)).save(any(Location.class));
+    }
+
+    @Test
+    void createLocation_whenCountryNameIsWhitespaceOnly_throwsInsufficientDataException() {
+        // when
+        Throwable result = catchThrowable(() -> locationCreateService.createLocation(new LocationDefinition(
+                "country-name",
+                0,
+                0,
+                "",
+                "   "
+        )));
+        // then
+        assertThat(result).isExactlyInstanceOf(InsufficientDataException.class);
+        verify(locationRepository, times(0)).save(any(Location.class));
+    }
+
+    @Test
     void createLocation_whenLongitudeValueIsInvalid_throwsInvalidDataException() {
         // when
         Throwable result = catchThrowable(() -> locationCreateService.createLocation(new LocationDefinition(
                 "city",
                 100,
                 0,
+                "",
+                "country"
+        )));
+        // then
+        assertThat(result).isExactlyInstanceOf(InvalidDataException.class);
+        verify(locationRepository, times(0)).save(any(Location.class));
+    }
+
+    @Test
+    void createLocation_whenLatitudeValueIsInvalid_throwsInvalidDataException() {
+        // when
+        Throwable result = catchThrowable(() -> locationCreateService.createLocation(new LocationDefinition(
+                "city",
+                0,
+                200,
                 "",
                 "country"
         )));
